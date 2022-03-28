a8dc7ab73d1e3c46ad2264570102f847afb0a358
LourF <95651556+LourF@users.noreply.github.com>
2022-03-28 17:13:32 +0100

    Update app.js

--- Ficha2/app.js
+++ Ficha2/app.js
@@ -110,23 +110,30 @@ function box(width, height){
 }
 
 //9
-// var student = {
+// var student1 = {
 //     number: 1000,
 //     grade: 16
 // };
 // var student2 = {
-//     number: 1001,
+//     number: 2000,
 //     grade: 12
 // };
-// var student3 = {
-//     number: 1002,
-//     grade: 14
+//     number: 4000,
+//     grade: 6
 // };
 // var students = [];
-// students.push(student);
-// students.push(student2)
-// students.push(student3)
 
+// students.push(student1);
+// students.push(student2);
+// students.push(student3);
+// students.push(student4);
+
+// console.log(students[0].number);
+
+var student ={
+    number: 1000,
+    grade: 16
+};
 
 var students = [];
 for (let i=0; i<10; i++){
@@ -157,13 +164,66 @@ function bestGrade(students){
 
 function worstGrade(students){
     var min = students[0].grade;
-    for (let i = 1; i < stunders.length; i++){
-        if(students[i].grade > min){
+    for (let i = 1; i < students.length; i++){
+        if(students[i].grade < min){
             min = students[i].grade;
             worstStudent = students[i];
         }
     }
-    console.log("worst Student - Number: " + worstStudent.number + "  //  Grade: " + worstStudent.grade)
+    console.log("Worst Student - Number: " + worstStudent.number + " // Grade: " + worstStudent.grade)
+}
+
+// function avarageGrade(students){ 
+//     for (let i = 0; i < students.length; i++) {
+//         med += students[i].grade;
+//         }
+//         med /= students.length;
+//         return med;
+//     }
+//      console.log(med2 + " grade is " + med1 + " avarage class grade is: " + medGrade)
+
+function average(students){
+    var avg = 0;
+    for (let i = 0; i < students.length; i++){
+        avg += students[i].grade;
+    }
+    return avg / students.length
+}
+
+
+function closestToAvg(students){
+    var avg= average(students);
+    var student = students[0];
+    var aux = Math.abs(avg - students[0].grade);
+    for (var i= 1; i< students.length; i++){
+        var diff= math.abs(avg - students[i].grade);{
+            if (diff < aux){
+            aux = diff;
+            student= students[i];
+            }
+        }
+    } 
+    return student;
+} 
+
+function badGrades(students){
+    var bad = 0;
+    for (let i = 0; i < students.length; i++){
+        if(students[i].grade < 9.5){
+            bad++;
+        }
+    }
+    return bad;
+}
+
+function goodGrades(students){
+    var good = 0;
+    for (let i = 0; i < students.length; i++){
+        if(students[i].grade > 9.5){
+            good++;
+        }
+    }
+    return good;
 }
 
 function main(students, option){
@@ -178,6 +238,15 @@ function main(students, option){
             worstGrade(students)
             break;
         case 4:
+            var student = closestToAvg(students);
+            console.log("Avarage grade - Number: " + student.number + " // Grade: " + student.grade) 
+            break;
+        case 5:
+            badGrades(students)
+            break;
+        case 6:
+            goodGrades(students)
+            break;
 
     }
 }
@@ -192,4 +261,4 @@ function main(students, option){
 // triangle(10);
 // box (3, 3);
 // console.log(students[0].number)
-main(students, 2);
+main(students, 4);
